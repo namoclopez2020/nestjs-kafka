a3943c8b6a3ba1e3e49b02f31c114e3120e04648
Jose Namoc <namoc.lopez.j@gmail.com>
2023-07-16 10:43:42 -0500

    added configuration for kafka client and emit an event

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,28 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get, Post, Logger, Inject, Body } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('Kafka')
+    private readonly kafka: ClientProxy
+  ) {}
 
   @MessagePattern('message.created')
   public messageCreate(@Payload() payload: any){
     Logger.log(payload, AppController.name)
+  }
+
+  @Post('/send')
+  public sendMessage(
+    @Body('message') message: string,
+    @Body('user') user: string
+  ) {
+    return this.kafka.emit('message.created', {
+      message, 
+      user
+    })
   }
 }
